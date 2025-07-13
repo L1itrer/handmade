6515566396bcdb16f5720b4bffcc90d293e5c553
Litrer <106829230+L1itrer@users.noreply.github.com>
2025-07-13 22:38:50 +0200

    Add creation of a window

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir build
 pushd build
-cl -Zi ..\src\handmade.cpp User32.lib
+cl -std:c++20 -Zi ..\src\handmade.cpp User32.lib Gdi32.lib
 popd
--- src/handmade.cpp
+++ src/handmade.cpp
@@ -1,7 +1,95 @@
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+
+
+#define GetAllMessages(msg) GetMessage(msg, 0, 0, 0)
+
+LRESULT CALLBACK MainWindowCallback(
+  HWND Window,
+  UINT Message,
+  WPARAM WParam,
+  LPARAM LParam 
+)
 {
-	MessageBoxA(0, "Hello from Windows!", "Handmade", 0);
+	LRESULT Result = 0;
+	switch (Message)
+	{
+		case WM_SIZE:
+			OutputDebugStringA("WM_SIZE\n");
+			break;
+		case WM_DESTROY:
+			OutputDebugStringA("WM_DESTROY\n");
+			break;
+		case WM_CLOSE:
+			OutputDebugStringA("WM_CLOSE\n");
+			break;
+		case WM_ACTIVATEAPP:
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+			break;
+		case WM_PAINT: {
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.bottom;
+			PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
+			EndPaint(Window, &Paint);
+		}
+			break;
+		default:
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+			break;
+	}
+	return Result;
+}
+
+int CALLBACK WinMain(
+	HINSTANCE Instance, 
+	HINSTANCE PrevInstance, 
+	LPSTR CommandLine, 
+	int ShowCode)
+{
+	WNDCLASS WindowClass = {
+		.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW,
+		.lpfnWndProc = MainWindowCallback,
+		.hInstance = Instance,
+		.lpszClassName = "HandmadeHeroWindowClass"
+	};
+
+	if (!RegisterClassA(&WindowClass))
+	{
+		MessageBoxA(0, "Registering the class failed", "There is no God", MB_OK | MB_ICONSTOP);
+		return 1;
+	}
+
+	HWND WindowHandle = CreateWindowExA(0, 
+									WindowClass.lpszClassName, 
+									"Handmade Hero", 
+									WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+									CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+									0, 0, Instance, 0);
+	if (!WindowHandle)
+	{
+		// TODO: im to lazy to report this it's not gonna fail
+	}
+	MSG Message;
+
+	for (;;)
+	{
+		BOOL MessageResult = GetAllMessages(&Message);
+		if (MessageResult > 0)
+		{
+			// NOTE: These functions can fail but if windows decides not to handle the messeges
+			// there is not much to be done so yeah
+			TranslateMessage(&Message);
+			DispatchMessage(&Message);	
+		}
+		else 
+		{
+			break;
+		}
+
+	}
 	return 0;
 }

f5fadd66fbd83f2b32c8e59c718d040b855a033f
Litrer <106829230+L1itrer@users.noreply.github.com>
2025-07-13 17:40:05 +0200

    Ready? Set! GO!

---
+++ src/handmade.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBoxA(0, "Hello from Windows!", "Handmade", 0);
+	return 0;
+}

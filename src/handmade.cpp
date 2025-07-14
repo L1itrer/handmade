7c5aab27c62ce23e149a80e51ddf0db698a1b95e
Litrer <106829230+L1itrer@users.noreply.github.com>
2025-07-14 15:45:56 +0200

    Prepare allocating the pixel buffer

--- src/handmade.cpp
+++ src/handmade.cpp
@@ -1,27 +1,76 @@
 #include <windows.h>
 
+static bool Running = true;
 
+#define GetMessageAll(msg) GetMessage(msg, 0, 0, 0)
 
-#define GetAllMessages(msg) GetMessage(msg, 0, 0, 0)
+BITMAPINFO BitmapInfo = {
+	.bmiHeader = {
+		.biSize = sizeof(BITMAPINFOHEADER),
+		.biPlanes = 1,
+		.biBitCount = 32,
+		.biCompression = BI_RGB,
+		.biSizeImage = 0,
+		.biXPelsPerMeter = 0,
+		.biYPelsPerMeter = 0,
+		.biClrUsed = 0,
+		.biClrImportant = 0
+	}
+};
+static void* BitmapMemory;
+static HBITMAP BitmapHandle; 
+static HDC BitmapDeviceContext;
+// DIB - Device Independent Bitmap
+void Win32ResizeDIBSection(int Width, int Height)
+{
+	if (BitmapHandle)
+	{
+		DeleteObject(BitmapHandle);
+	}
+	else
+	{
+		BitmapDeviceContext = CreateCompatibleDC(0);
+	}
 
-LRESULT CALLBACK MainWindowCallback(
+	
+	BitmapInfo.bmiHeader.biHeight = Height;
+	BitmapInfo.bmiHeader.biWidth = Width;
+	HDC DeviceContext = CreateCompatibleDC(0);
+	BitmapHandle = CreateDIBSection(BitmapDeviceContext, &BitmapInfo, DIB_RGB_COLORS, &BitmapMemory, 0, 0);
+}
+
+void Win32WindowUpdate(HDC DeviceContext, int X, int Y, int W, int H)
+{
+	StretchDIBits(
+		DeviceContext,
+		X, Y, W, H,
+		X, Y, W, H,
+		&BitmapMemory, &BitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+		
+}
+
+LRESULT CALLBACK Win32MainWindowCallback(
   HWND Window,
   UINT Message,
   WPARAM WParam,
-  LPARAM LParam 
-)
+  LPARAM LParam)
 {
 	LRESULT Result = 0;
 	switch (Message)
 	{
-		case WM_SIZE:
-			OutputDebugStringA("WM_SIZE\n");
+		case WM_SIZE: {
+			RECT ClientRect;
+			GetClientRect(Window, &ClientRect);
+			int Width = ClientRect.right - ClientRect.left;
+			int Height = ClientRect.bottom - ClientRect.top;
+			Win32ResizeDIBSection(Width, Height);
+		}
 			break;
 		case WM_DESTROY:
-			OutputDebugStringA("WM_DESTROY\n");
+			Running = false;
 			break;
 		case WM_CLOSE:
-			OutputDebugStringA("WM_CLOSE\n");
+			Running = false;
 			break;
 		case WM_ACTIVATEAPP:
 			OutputDebugStringA("WM_ACTIVATEAPP\n");
@@ -32,7 +81,8 @@ LRESULT CALLBACK MainWindowCallback(
 			int X = Paint.rcPaint.left;
 			int Y = Paint.rcPaint.top;
 			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-			int Height = Paint.rcPaint.bottom - Paint.rcPaint.bottom;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			Win32WindowUpdate(DeviceContext, X, Y, Width, Height);
 			PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
 			EndPaint(Window, &Paint);
 		}
@@ -52,7 +102,7 @@ int CALLBACK WinMain(
 {
 	WNDCLASS WindowClass = {
 		.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW,
-		.lpfnWndProc = MainWindowCallback,
+		.lpfnWndProc = Win32MainWindowCallback,
 		.hInstance = Instance,
 		.lpszClassName = "HandmadeHeroWindowClass"
 	};
@@ -73,22 +123,17 @@ int CALLBACK WinMain(
 	{
 		// TODO: im to lazy to report this it's not gonna fail
 	}
-	MSG Message;
 
-	for (;;)
+	while (Running)
 	{
-		BOOL MessageResult = GetAllMessages(&Message);
-		if (MessageResult > 0)
-		{
-			// NOTE: These functions can fail but if windows decides not to handle the messeges
-			// there is not much to be done so yeah
-			TranslateMessage(&Message);
-			DispatchMessage(&Message);	
-		}
-		else 
-		{
-			break;
-		}
+		MSG Message;
+		BOOL MessageResult = GetMessageAll(&Message);
+		if (MessageResult <= 0) break;
+
+		// NOTE: These functions can fail but if windows decides not to handle the messeges
+		// there is not much to be done so yeah
+		TranslateMessage(&Message);
+		DispatchMessageA(&Message);	
 
 	}
 	return 0;
